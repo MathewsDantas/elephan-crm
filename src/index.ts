76f10b98e48d4cfda79d89dc2f6421d5556e87ba
Mathews Dantas <94187504+MathewsDantas@users.noreply.github.com>
2024-11-10 17:00:16 -0300

    feat: adição do middleware CORS para permitir requisições de diferentes origens

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
+import cors from 'cors';
+
 import config from './config/config';
 import app from './app';
 import { logger } from './config/logger';
+
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
 
 app.listen(config.PORT, () => {
   logger.info(`Server está rodando em http://localhost:${config.PORT}`);
